ffd30cfa9196bc8d499b5a7923bce813a51af94b
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-07 08:21:33 +0900

    バグ

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-
 import './App.css';
 import axios from 'axios';
+import {useState} from "react";
+import { Todo } from './Todo';
 
 function App() {
+  const [todos, setTodos] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) =>{
-      console.log(res)
+      setTodos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得ハゲタコ</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid}/>
+      ))}
     </div>
   );
 }
-
 export default App;
